a01e7b5af36ad980670280cbddcad5d40671138b
Raul Espinoza M <raul.espmol@gmail.com>
2023-11-21 18:50:28 -0300

    estilos pass correcto/incorrecto

--- assets/script/password.js
+++ assets/script/password.js
@@ -8,9 +8,15 @@ function verificarPassword() {
   let password = A.value + B.value + C.value;
   if (password === '911') {
     mensaje.innerText = 'Password 1 correcto';
+    mensaje.classList.add('ok');
+    mensaje.classList.remove('error');
   } else if (password === '714') {
     mensaje.innerText = 'Password 2 correcto';
+    mensaje.classList.add('ok');
+    mensaje.classList.remove('error');
   } else {
     mensaje.innerText = 'Password incorrecto';
+    mensaje.classList.remove('ok');
+    mensaje.classList.add('error');
   }
 }
