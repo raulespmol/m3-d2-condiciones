e00f60bac04446f7f43428abe0720b9d1b1521b8
Raul Espinoza M <raul.espmol@gmail.com>
2023-11-23 20:52:30 -0300

    personalizar estilos

--- assets/script/stickers.js
+++ assets/script/stickers.js
@@ -5,8 +5,18 @@ const stickerC = document.querySelector('#sticker3');
 const mensaje = document.querySelector('#mensaje');
 
 function contarStickers(){
-  let totalStickers = Number(stickerA.value) + Number(stickerB.value) + Number(stickerC.value);
-  if(totalStickers <= 10 ){
+  let totalStickers = 
+    Number(stickerA.value)
+    + Number(stickerB.value)
+    + Number(stickerC.value);
+
+  if (totalStickers == 0) {
+    mensaje.innerText = 'No llevas ningún sticker';
+    mensaje.classList.remove('error');
+  } else if(totalStickers == 1) {
+    mensaje.innerText = 'Llevas sólo 1 sticker';
+    mensaje.classList.remove('error');
+  } else if(totalStickers <= 10 ) {
     mensaje.innerText = `Llevas ${totalStickers} stickers`;
     mensaje.classList.remove('error');
   } else {
